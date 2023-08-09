2b1f5c2998ba9fb87de3e8529c76d59ef4b0b8d0
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-08-09 11:48:53 +0630

    From "Start" To "Store Blog Posts as HTML Files".

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/app.css" />
+</head>
+<body>
+    <article>
+        <h1><a href="/posts/my-first-post">My First Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id rerum natus aperiam amet, officiis odit eveniet. Ipsa, nulla perspiciatis debitis aut officiis reiciendis. Sit eos quae tempore, nihil laborum dignissimos sint molestiae earum eveniet cumque repellendus amet perspiciatis ullam hic consequuntur vitae, sunt eum deleniti pariatur libero esse! Laborum, minus?</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id rerum natus aperiam amet, officiis odit eveniet. Ipsa, nulla perspiciatis debitis aut officiis reiciendis. Sit eos quae tempore, nihil laborum dignissimos sint molestiae earum eveniet cumque repellendus amet perspiciatis ullam hic consequuntur vitae, sunt eum deleniti pariatur libero esse! Laborum, minus?</p>
+    </article>
+
+    <article>
+        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
+
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id rerum natus aperiam amet, officiis odit eveniet. Ipsa, nulla perspiciatis debitis aut officiis reiciendis. Sit eos quae tempore, nihil laborum dignissimos sint molestiae earum eveniet cumque repellendus amet perspiciatis ullam hic consequuntur vitae, sunt eum deleniti pariatur libero esse! Laborum, minus?</p>
+    </article>
+</body>
+</html>
