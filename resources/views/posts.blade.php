5372aa8016282947218cb293bf0d54f8ffd31e9d
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-08-09 15:46:38 +0630

    Use the Filesystem Class to Read a Directory.

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -4,25 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="/app.css" />
+    <link rel="stylesheet" href="/app.css"/>
 </head>
 <body>
-    <article>
-        <h1><a href="/posts/my-first-post">My First Post</a></h1>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id rerum natus aperiam amet, officiis odit eveniet. Ipsa, nulla perspiciatis debitis aut officiis reiciendis. Sit eos quae tempore, nihil laborum dignissimos sint molestiae earum eveniet cumque repellendus amet perspiciatis ullam hic consequuntur vitae, sunt eum deleniti pariatur libero esse! Laborum, minus?</p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-second-post">My Second Post</a></h1>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id rerum natus aperiam amet, officiis odit eveniet. Ipsa, nulla perspiciatis debitis aut officiis reiciendis. Sit eos quae tempore, nihil laborum dignissimos sint molestiae earum eveniet cumque repellendus amet perspiciatis ullam hic consequuntur vitae, sunt eum deleniti pariatur libero esse! Laborum, minus?</p>
-    </article>
-
-    <article>
-        <h1><a href="/posts/my-third-post">My Third Post</a></h1>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Id rerum natus aperiam amet, officiis odit eveniet. Ipsa, nulla perspiciatis debitis aut officiis reiciendis. Sit eos quae tempore, nihil laborum dignissimos sint molestiae earum eveniet cumque repellendus amet perspiciatis ullam hic consequuntur vitae, sunt eum deleniti pariatur libero esse! Laborum, minus?</p>
-    </article>
+<?php foreach ($posts as $post): ?>
+<article>
+    <?= $post ?>
+</article>
+<?php endforeach; ?>
 </body>
 </html>
