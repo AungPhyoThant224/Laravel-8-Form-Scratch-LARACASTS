8a2ab2b8ed7fc076ba11b7fc3becacab02756de5
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-08-09 21:40:17 +0630

    Blade Layouts Two Ways.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,23 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    <link rel="stylesheet" href="/app.css"/>
-</head>
-<body>
-<article>
-    <h1>
-        <a href="/posts/{{$post->slug}}">
-            {{$post->title}}
-        </a>
-    </h1>
+@extends('layout')
 
-    <div>{!! $post->body !!}</div>
-</article>
+@section('content')
+    <article>
+        <h1>
+            <a href="/posts/{{$post->slug}}">
+                {{$post->title}}
+            </a>
+        </h1>
 
-<a href="/"> < Go Back</a>
-</body>
-</html>
+        <div>{!! $post->body !!}</div>
+    </article>
+
+    <a href="/">< Go Back</a>
+@endsection
