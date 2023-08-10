dfbfb2742b2f6e8fdb207feacd42e4d92f27e2f0
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-08-10 16:46:23 +0630

    Turbo Boost With Factories.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,44 +16,18 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
 
-        User::truncate();
-        Post::truncate();
-        Category::truncate();
-
-         $user = User::factory()->create();
-
-         $personal = Category::create([
-             'name' => 'Personal',
-             'slug' => 'personal',
+         $user = User::factory()->create([
+             'name' => 'John Doe'
          ]);
 
-         $family = Category::create([
-             'name' => 'Family',
-             'slug' => 'family',
-         ]);
-
-         $work = Category::create([
-             'name' => 'Work',
-             'slug' => 'work',
-         ]);
-
-         Post::create([
+         Post::factory(5)->create([
              'user_id' => $user->id,
-             'category_id' => $personal->id,
-             'title' => 'My First Post',
-             'slug' => 'my-first-post',
-             'excerpt' => "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>",
-             'body' => "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>"
          ]);
 
-         Post::create([
-             'user_id' => 1,
-             'category_id' => $work->id,
-             'title' => 'My Second Post',
-             'slug' => 'my-second-post',
-             'excerpt' => "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>",
-             'body' => "<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</p>"
-         ]);
+//         $personal = Category::create([
+//             'name' => 'Personal',
+//             'slug' => 'personal',
+//         ]);
 
     }
 }
