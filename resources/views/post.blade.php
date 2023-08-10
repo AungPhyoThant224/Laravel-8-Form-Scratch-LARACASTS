c8517d794cd1cdeebc73357ca6e1b10cac6d3cac
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-08-10 14:35:06 +0630

    Show All Posts Associated With a Category.

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -6,7 +6,7 @@
             </a>
         </h1>
 
-        <p><a href="#">{{$post->category->name}}</a></p>
+        <p><a href="/categories/{{$post->category->slug}}">{{$post->category->name}}</a></p>
 
         <div>{!! $post->body !!}</div>
     </article>
