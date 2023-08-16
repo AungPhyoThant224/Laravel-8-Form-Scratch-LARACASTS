d0c6c71cbf4ed902f1c59989107a259ee6dcf87d
Aung Phyo Thant <aungphyothant224@gmail.com>
2023-08-16 16:01:29 +0630

    Some Light Chapter Clean Up.

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -13,7 +13,8 @@
                     <div class="flex items-center lg:justify-center text-sm mt-4">
                         <img src="/images/lary-avatar.svg" alt="Lary avatar"/>
                         <div class="ml-3 text-left">
-                            <h5 class="font-bold"><a href="/?author={{$post->author->username}}">{{$post->author->name}}</a></h5>
+                            <h5 class="font-bold"><a
+                                    href="/?author={{$post->author->username}}">{{$post->author->name}}</a></h5>
                         </div>
                     </div>
                 </div>
@@ -43,7 +44,7 @@
                         </a>
 
                         <div class="space-x-2">
-                            <x-category-button :category="$post->category" />
+                            <x-category-button :category="$post->category"/>
                         </div>
                     </div>
 
@@ -57,32 +58,10 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
-                    @auth
-                        <x-panel>
-                            <form method="POST" action="/posts/{{$post->slug}}/comments" >
-                                @csrf
-                                <header class="flex items-center">
-                                    <img src="https://i.pravatar.cc/60?u={{auth()->id()}}" alt="" width="40" height="40" class="rounded-full"/>
-                                    <h2>Want to participate?</h2>
-                                </header>
-
-                                <div class="mt-6">
-                                    <textarea name="body" class="p-1 w-full text-sm focus::outline-none focus::ring" rows="5" placeholder="Quick, thing of something to say." ></textarea>
-                                </div>
-
-                                <div class="flex justify-end mt-6 pt-6 border-t border-gray-200">
-                                    <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
-                                </div>
-                            </form>
-                        </x-panel>
-                    @else
-                        <p class="font-semibold">
-                            <a href="/register" class="hover:underline">Register</a> or <a href="/login" class="hover:underline">Log In</a> to leave a comment.
-                        </p>
-                    @endauth
+                    @include('posts._add-comment-form')
 
                     @foreach($post->comments as $comment)
-                        <x-post-comment :comment="$comment" />
+                        <x-post-comment :comment="$comment"/>
                     @endforeach
                 </section>
 
